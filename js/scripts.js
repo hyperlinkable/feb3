a10ba081da941b5df0e3ba3155a5d07d7f85ff61
David Wynn and Molly McGlone <davidwwynn@gmail.com>
2016-02-03 19:19:04 -0800

    add a fadeIn for walrus

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     $("#initially_showing").toggle();
     $("#initially_hidden").toggle();
   });
+
+  $(".clickable2").click(function() {
+    $("#walrus").fadeToggle(2000);
+  });
+
 });
